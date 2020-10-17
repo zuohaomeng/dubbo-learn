7288941d1d816177aa1a3bf9f29cdaa7d149d848
左浩 <hao.zuo2@zhenai.com>
2020-10-17 13:32:56 +0800

    Initial commit

---
+++ dubbo-provider/src/test/java/com/meng/dubboprovider/DubboProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.dubboprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
